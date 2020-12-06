dd0a6bcf12d908d709a47295ef2270464f516890
Mufti Alie S <iyakiwan19@gmail.com>
2020-12-06 12:22:38 +0700

    Fix Arus Kas

--- app/Http/Controllers/ArusKasController.php
+++ app/Http/Controllers/ArusKasController.php
@@ -151,14 +151,11 @@ class ArusKasController extends Controller
         
         try {
             $arus_kas_detail = Arus_Kas::select('created_at as tanggal', 'arus', 'nama', 
-                    'keterangan', 'total_biaya as biaya', 'status')->get();
-                    // Arus_Kas::select('created_at as tanggal', 'arus', 'nama', 
-                    // 'keterangan', 'total_biaya as biaya', 'status')
-                    // ->where('status', '=', 'diterima')
-                    // // ->whereMonth('created_at', '=', $request->sort_month)
-                    // // ->whereYear('created_at', '=', $request->sort_year)
-                    // ->get();
-            dd($arus_kas_detail);
+                    'keterangan', 'total_biaya as biaya', 'status')
+                    ->where('status', '=', 'diterima')
+                    ->whereMonth('created_at', '=', $request->sort_month)
+                    ->whereYear('created_at', '=', $request->sort_year)
+                    ->get();
             $arus_kas_masuk = DB::table('arus_kas')
                     ->select(DB::raw('SUM(total_biaya) as ttl_masuk'))
                     ->where([['status', '=', 'diterima'],['arus', '=', 'masuk']])
